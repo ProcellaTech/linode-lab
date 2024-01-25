467a74c56ad5ba7d278485097252fefaf3d8b2cd
John Payne <john@procella.tech>
2024-01-25 12:44:38 -0500

    create billing and crm apps

---
+++ billing.tf
@@ -0,0 +1,175 @@
+# app server is wordpress
+
+resource "linode_instance" "billing_app" {
+  image = var.image
+  label = "billing_app"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.billing_app_root.password
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_wordpress.tftpl", {label="billing-app",wppw=onepassword_item.billing_db.password,gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password,mysqlip=local.billing_db_ip,wordpressname="billing-app.${var.domain}", proxy=local.proxy_ip}))
+  }
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+
+
+resource "linode_instance" "billing_db" {
+  image = var.image
+  label = "billing_db"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.billing_db_root.password
+
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_mysql.tftpl", {label="billing-db",wppw=onepassword_item.billing_db.password,gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password, proxy=local.proxy_ip}))
+  }
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+resource "linode_instance" "billing_web" {
+  image = var.image
+  label = "billing_web"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.billing_web_root.password
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_nginx.tftpl", {label="billing-web",gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password,wordpressip=local.billing_app_ip, wordpressname="billing-app.${var.domain}",proxy=local.proxy_ip}))
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+
+
+data "linode_instances" "billing_app" {
+  depends_on = [linode_instance.billing_app]
+  filter {
+    name = "label"
+    values = [ "billing_app" ]
+  }
+}
+
+data "linode_instances" "billing_web" {
+  depends_on = [linode_instance.billing_web]
+  filter {
+    name = "label"
+    values = [ "billing_web" ]
+  }
+}
+
+data "linode_instances" "billing_db" {
+  depends_on = [linode_instance.billing_db]
+  filter {
+    name = "label"
+    values = [ "billing_db" ]
+  }
+}
+
+
+
+
+
+
+# variables to hold IP addresses
+locals {
+  billing_app_ip = data.linode_instances.billing_app.instances.0.config.0.interface.1.ipv4.0.vpc
+  billing_web_ip = data.linode_instances.billing_web.instances.0.config.0.interface.1.ipv4.0.vpc
+  billing_db_ip = data.linode_instances.billing_db.instances.0.config.0.interface.1.ipv4.0.vpc
+}
+
+
+resource "onepassword_item" "billing_app_root" {
+  vault = var.Linode-lab
+
+  title    = "billing_app root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "billing_db_root" {
+  vault = var.Linode-lab
+
+  title    = "billing_db root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "billing_web_root" {
+  vault = var.Linode-lab
+
+  title    = "billing_web root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "billing_db" {
+  vault = var.Linode-lab
+
+  title    = "billing_db password"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
---
+++ crm.tf
@@ -0,0 +1,175 @@
+# app server is wordpress
+
+resource "linode_instance" "crm_app" {
+  image = var.image
+  label = "crm_app"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.crm_app_root.password
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_wordpress.tftpl", {label="crm-app",wppw=onepassword_item.crm_db.password,gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password,mysqlip=local.crm_db_ip,wordpressname="crm-app.${var.domain}", proxy=local.proxy_ip}))
+  }
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+
+
+resource "linode_instance" "crm_db" {
+  image = var.image
+  label = "crm_db"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.crm_db_root.password
+
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_mysql.tftpl", {label="crm-db",wppw=onepassword_item.crm_db.password,gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password, proxy=local.proxy_ip}))
+  }
+  
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+resource "linode_instance" "crm_web" {
+  image = var.image
+  label = "crm_web"
+  region = var.region
+  type = var.type
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.crm_web_root.password
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+  user_data = base64encode(templatefile("${path.module}/install_nginx.tftpl", {label="crm-web",gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password,wordpressip=local.crm_app_ip, wordpressname="crm-app.${var.domain}",proxy=local.proxy_ip}))
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "./delete_agent.py ${self.label}"
+  }
+  
+}
+
+
+
+data "linode_instances" "crm_app" {
+  depends_on = [linode_instance.crm_app]
+  filter {
+    name = "label"
+    values = [ "crm_app" ]
+  }
+}
+
+data "linode_instances" "crm_web" {
+  depends_on = [linode_instance.crm_web]
+  filter {
+    name = "label"
+    values = [ "crm_web" ]
+  }
+}
+
+data "linode_instances" "crm_db" {
+  depends_on = [linode_instance.crm_db]
+  filter {
+    name = "label"
+    values = [ "crm_db" ]
+  }
+}
+
+
+
+
+
+
+# variables to hold IP addresses
+locals {
+  crm_app_ip = data.linode_instances.crm_app.instances.0.config.0.interface.1.ipv4.0.vpc
+  crm_web_ip = data.linode_instances.crm_web.instances.0.config.0.interface.1.ipv4.0.vpc
+  crm_db_ip = data.linode_instances.crm_db.instances.0.config.0.interface.1.ipv4.0.vpc
+}
+
+
+resource "onepassword_item" "crm_app_root" {
+  vault = var.Linode-lab
+
+  title    = "crm_app root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "crm_db_root" {
+  vault = var.Linode-lab
+
+  title    = "crm_db root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "crm_web_root" {
+  vault = var.Linode-lab
+
+  title    = "crm_web root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+resource "onepassword_item" "crm_db" {
+  vault = var.Linode-lab
+
+  title    = "crm_db password"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
