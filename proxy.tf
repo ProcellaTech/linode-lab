42031915930eada2373eb14479e663dffff6509d
John Payne <john@procella.tech>
2024-01-25 12:36:01 -0500

    move proxy into it's own file

---
+++ proxy.tf
@@ -0,0 +1,80 @@
+# I wanted to have no NAT 1:1 but there's no cloud gateway, I can't build
+# my own nat gateway and i can't access the metadata service to configure
+# proxies without having the proxy configured.  so we're going to go with 
+# nat 1:1 for now.  Never mind - i need a public IP for metadata service
+
+# I also need to find a way to extract the vpc IP assigned so i don't have
+# to statically assign (and we can use DNS records instead)
+
+
+# ok, so we can boot up with public interface, in the metadata tell guardicore
+# agent to use a proxy and then disable the public interface elsewhere.  Let's 
+# see if it works!
+
+# the actual proxy
+
+resource "linode_instance" "lab_proxy" {
+  image = var.image
+  label = "proxy"
+  region = var.region
+  type = var.type
+  
+  authorized_keys = [ linode_sshkey.procellab_sshkey.ssh_key ]
+  root_pass = onepassword_item.proxy_root.password
+
+  interface {
+    purpose = "public"
+  }
+
+  interface {
+    purpose = "vpc"
+    subnet_id = linode_vpc_subnet.gc-procellab.id
+  }
+
+  metadata  {
+    user_data = base64encode(templatefile("${path.module}/install_proxy.tftpl", {label="proxy",gcagg=var.gcagg_ip,gcuium=data.onepassword_item.gcuium.password}))
+  }
+}
+
+
+
+# used to pull out the IP address for use by other things
+
+data "linode_instances" "proxy" {
+  depends_on = [linode_instance.lab_proxy]
+  filter {
+    name = "label"
+    values = [ "proxy" ]
+  }
+}
+
+
+# a local "variable" to hold the proxy IP address
+locals {
+  proxy_ip = data.linode_instances.proxy.instances.0.config.0.interface.1.ipv4.0.vpc
+}
+
+# create a DNS record for proxy.linode.procellab.zone pointing to the VPC IP
+resource "linode_domain_record" "proxy_dns_record" {
+  domain_id = "${linode_domain.procellab_domain.id}"
+  name = "proxy"
+  record_type = "A"
+  ttl_sec = 30
+  target = "${local.proxy_ip}"
+}
+
+
+# generate a unique password for root and store it in 1password
+resource "onepassword_item" "proxy_root" {
+  vault = var.Linode-lab
+
+  title    = "Proxy root"
+  category = "password"
+
+  password_recipe {
+    length  = 40
+    symbols = false
+  }
+}
+
+
